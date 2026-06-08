ebba92abce91c80a58d6e4ec514c6329a330bc03
root <klever1988@protonmail.com>
2026-06-09 00:26:50 +0800

    fix: add app icon to tray icon

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -163,6 +163,7 @@ pub fn run() {
 
             let _tray = TrayIconBuilder::new()
                 .menu(&menu)
+                .icon(app.default_window_icon().unwrap().clone())
                 .on_menu_event(move |app, event| match event.id().as_ref() {
                     "show" => {
                         if let Some(w) = app.get_webview_window("main") {
